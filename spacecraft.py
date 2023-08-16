f34893de14b36b509ad34b182c307e72aa03a301
vatsalgandhi83 <81321485+vatsalgandhi83@users.noreply.github.com>
2023-08-16 13:58:02 +0530

    Update spacecraft.py
    Added more functions to maintain directions when up, down or in plane

--- spacecraft.py
+++ spacecraft.py
@@ -4,33 +4,36 @@ class Spacecraft:
         self.y = y
         self.z = z
         self.direction = direction
+        self.angle = "Plane"
 
     def move_forward(self):
-        if self.direction == "North":
-            self.y += 1
-        elif self.direction == "South":
-            self.y -= 1
-        elif self.direction == "East":
-            self.x += 1
-        elif self.direction == "West":
-            self.x -= 1
+        if self.angle == "Plane":
+            if self.direction == "North":
+                self.y += 1
+            elif self.direction == "South":
+                self.y -= 1
+            elif self.direction == "East":
+                self.x += 1
+            elif self.direction == "West":
+                self.x -= 1
         elif self.direction == "Up":
             self.z += 1
         elif self.direction == "Down":
             self.z -= 1
 
     def move_backward(self):
-        if self.direction == "North":
-            self.y -= 1
-        elif self.direction == "South":
-            self.y += 1
-        elif self.direction == "East":
-            self.x -= 1
-        elif self.direction == "West":
-            self.x += 1
-        elif self.direction == "Up":
+        if self.angle == "Plane":
+            if self.direction == "North":
+                self.y -= 1
+            elif self.direction == "South":
+                self.y += 1
+            elif self.direction == "East":
+                self.x -= 1
+            elif self.direction == "West":
+                self.x += 1
+        elif self.angle == "Up":
             self.z -= 1
-        elif self.direction == "Down":
+        elif self.angle == "Down":
             self.z += 1
 
     def turn_left(self):
@@ -54,12 +57,16 @@ class Spacecraft:
             self.direction = "North"
 
     def turn_up(self):
-        if self.direction in ["North", "South", "East", "West"]:
-            self.direction = "Up"
+        if self.angle == "Plane":
+            self.angle = "Up"
+        elif self.angle == "Down":
+            self.angle = "Plane"
 
     def turn_down(self):
-        if self.direction in ["North", "South", "East", "West"]:
-            self.direction = "Down"
+        if self.angle == "Plane":
+            self.angle = "Down"
+        elif self.angle == "Up":
+            self.angle == "Plane"
 
     def execute_commands(self, commands):
         for command in commands:
