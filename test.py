1cc4f92e17aaedc2dd5134a9b4d829dd08d8e6a8
vatsalgandhi83 <81321485+vatsalgandhi83@users.noreply.github.com>
2023-08-16 13:07:32 +0530

    Update test.py
    Added more tests. And on the basis of the tests, I'm updating the functionalities (member functions) of the spacecraft class

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 import unittest
-from spacecraft import Spacecraft # Importing Spacecraft class
+from spacecraft import Spacecraft
 
 
 class TestSpacecraft(unittest.TestCase):
     def test_move_forward_north(self):
-        spacecraft = Spacecraft(0, 0, 0, "N")
+        starting_position = (0, 0, 0)
+        initial_direction = "North"
+        spacecraft = Spacecraft(*starting_position, initial_direction)
         spacecraft.move_forward()
         self.assertEqual(spacecraft.y, 1)
 
-TestSpacecraft.test_move_forward_north()
+    def test_move_forward_south(self):
+        starting_position = (0, 0, 0)
+        initial_direction = "South"
+        spacecraft = Spacecraft(*starting_position, initial_direction)
+        spacecraft.move_forward()
+        self.assertEqual(spacecraft.y, -1)
+
+    def test_move_forward_east(self):
+        starting_position = (0, 0, 0)
+        initial_direction = "East"
+        spacecraft = Spacecraft(*starting_position, initial_direction)
+        spacecraft.move_forward()
+        self.assertEqual(spacecraft.x, 1)
+
+    def test_move_forward_west(self):
+        starting_position = (0, 0, 0)
+        initial_direction = "West"
+        spacecraft = Spacecraft(*starting_position, initial_direction)
+        spacecraft.move_forward()
+        self.assertEqual(spacecraft.x, -1)
+
+if __name__ == '__main__':
+    unittest.main()
