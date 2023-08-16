ac9190cd064df9b04a12a0fcaf40ff43884e402c
vatsalgandhi83 <81321485+vatsalgandhi83@users.noreply.github.com>
2023-08-16 13:05:57 +0530

    Update spacecraft.py
    Added all the functionalities of the spacecraft

--- spacecraft.py
+++ spacecraft.py
@@ -19,6 +19,48 @@ class Spacecraft:
         elif self.direction == "Down":
             self.z -= 1
 
+    def move_backward(self):
+        if self.direction == "North":
+            self.y -= 1
+        elif self.direction == "South":
+            self.y += 1
+        elif self.direction == "East":
+            self.x -= 1
+        elif self.direction == "West":
+            self.x += 1
+        elif self.direction == "Up":
+            self.z -= 1
+        elif self.direction == "Down":
+            self.z += 1
+
+    def turn_left(self):
+        if self.direction == "North":
+            self.direction = "West"
+        elif self.direction == "South":
+            self.direction = "East"
+        elif self.direction == "East":
+            self.direction = "North"
+        elif self.direction == "West":
+            self.direction = "South"
+
+    def turn_right(self):
+        if self.direction == "North":
+            self.direction = "East"
+        elif self.direction == "South":
+            self.direction = "West"
+        elif self.direction == "East":
+            self.direction = "South"
+        elif self.direction == "West":
+            self.direction = "North"
+
+    def turn_up(self):
+        if self.direction in ["North", "South", "East", "West"]:
+            self.direction = "Up"
+
+    def turn_down(self):
+        if self.direction in ["North", "South", "East", "West"]:
+            self.direction = "Down"
+
     def execute_commands(self, commands):
         for command in commands:
             if command == "f":
